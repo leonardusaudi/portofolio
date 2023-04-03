0884184f03a88de85a3dc361f681e03a9bd50847
Wibianto <leonardus.wibianto@accenture.com>
2023-04-03 16:17:57 +0700

    add frammer motion

--- src/components/main.js
+++ src/components/main.js
@@ -10,8 +10,8 @@ const boxVariant = {
 };
 
 const boxVariant1 = {
-  hidden: { opacity: 0, x: "100vw" },
-  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: 0 },
+  visible: { opacity: 1, x: "-2vw" },
 };
 
 const Main = () => {
